8538749d23a88963da4fc113345bb23539afa34a
Youtirsin <171122318@qq.com>
2019-07-16 15:55:24 +0800

    login window

--- login/login.js
+++ login/login.js
@@ -10,6 +10,11 @@ function activate() {
                 'opacity': '1',
                 'transition': '0.5s'
             });
+            $('#account').animate({
+                'top': '-60px'
+            }, 500, function () {
+                $('#container').append('<div><input type="password"></div>');
+            });
             done = true;
         }
     });
